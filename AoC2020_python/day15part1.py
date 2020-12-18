a470105fe88b1fb4a82a032f326c39385977f4ca
CrepeGoat <awqatty.b@gmail.com>
2020-12-18 12:02:15 -0500

    finished day15 part1

---
+++ AoC2020_python/day15part1.py
@@ -0,0 +1,28 @@
+import itertools
+
+
+def play_the_game(*starters):
+    yield from starters
+
+    recents = {
+        s: i
+        for i, s in enumerate(starters[:-1], 1)
+    }
+
+    last_item = starters[-1]
+    for counter in itertools.count(len(starters)):
+        if last_item in recents:
+            next_item = counter - recents[last_item]
+        else:
+            next_item = 0
+        yield next_item
+        recents[last_item] = counter
+        last_item = next_item
+
+
+def get_2020th(*starters):
+    return next(
+        value
+        for (i, value) in enumerate(play_the_game(*starters), 1)
+        if i == 2020
+    )
