cda2e55b6993780df808083f44d2adbf6421c3e8
CrepeGoat <awqatty.b@gmail.com>
2020-12-21 19:46:57 -0500

    tmp

--- AoC2020-rust/src/bin/day12part1.rs
+++ AoC2020-rust/src/bin/day12part1.rs
@@ -14,12 +14,9 @@ enum ParseDirectionError {
 
 #[derive(Debug, PartialEq, Eq)]
 enum Direction {
-	North(u64),
-	South(u64),
-	East(u64),
-	West(u64),
+	North(i64),
+	East(i64),
 	Left(u16),
-	Right(u16),
 	Forward(u64),
 }
 
@@ -27,7 +24,7 @@ impl FromStr for Direction {
 	type Err = ParseDirectionError;
 
 	fn from_str(text: &str) -> Result<Self, Self::Err> {
-		use Direction::{North, South, East, West, Left, Right, Forward};
+		use Direction::{North, East, Left, Forward};
 		use ParseDirectionError as ErrType;
 
 		fn parse_int<I: FromStr<Err=ParseIntError>>(instr_text: &str) -> Result<I, ErrType> {
@@ -35,20 +32,20 @@ impl FromStr for Direction {
 		}
 
 		match text.chars().next().ok_or(ErrType::InstructionType)?.to_ascii_uppercase() {
-			'N' => Ok(North(parse_int::<u64>(&text[1..])?)),
-			'S' => Ok(South(parse_int::<u64>(&text[1..])?)),
-			'E' => Ok(East(parse_int::<u64>(&text[1..])?)),
-			'W' => Ok(West(parse_int::<u64>(&text[1..])?)),
+			'N' => Ok(North(parse_int::<u64>(&text[1..])? as i64)),
+			'S' => Ok(North(-(parse_int::<u64>(&text[1..])? as i64))),
+			'E' => Ok(East(parse_int::<u64>(&text[1..])? as i64)),
+			'W' => Ok(East(-(parse_int::<u64>(&text[1..])? as i64))),
 			'L' => Ok(Left(match parse_int::<u16>(&text[1..])? {
 				90 => Ok(90),
 				180 => Ok(180),
 				270 => Ok(270),
 				_val => Err(Self::Err::RotationValue),
 			}?)),
-			'R' => Ok(Right(match parse_int::<u16>(&text[1..])? {
-				90 => Ok(90),
+			'R' => Ok(Left(match parse_int::<u16>(&text[1..])? {
+				90 => Ok(270),
 				180 => Ok(180),
-				270 => Ok(270),
+				270 => Ok(90),
 				_val => Err(Self::Err::RotationValue),
 			}?)),
 			'F' => Ok(Forward(parse_int::<u64>(&text[1..])?)),
@@ -66,7 +63,7 @@ fn parse_input(input_seq: &str) -> Result<Vec<Direction>, ParseDirectionError> {
 fn calc_path_offset(path: Vec<Direction>, init_heading: Complex<i64>)
 -> (Complex<i64>, Complex<i64>)
 {
-	use Direction::{North, South, East, West, Left, Right, Forward};
+	use Direction::{North, East, Left, Forward};
 	type Coords = Complex<i64>;
 
 	let mut heading = init_heading;
@@ -74,12 +71,9 @@ fn calc_path_offset(path: Vec<Direction>, init_heading: Complex<i64>)
 
 	for instruction in path {
 		match instruction {
-			North(dist) => {position += dist as i64 * Coords::new(0, 1);},
-			South(dist) => {position += dist as i64 * Coords::new(0, -1);},
-			East(dist) => {position += dist as i64 * Coords::new(1, 0);},
-			West(dist) => {position += dist as i64 * Coords::new(-1, 0);},
+			North(dist) => {position += dist * Coords::new(0, 1);},
+			East(dist) => {position += dist * Coords::new(1, 0);},
 			Left(rot) => {heading *= Coords::new(0, 1).powu((rot as u32) / 90);},
-			Right(rot) => {heading *= Coords::new(0, -1).powu((rot as u32) / 90);},
 			Forward(dist) => {position += dist as i64 * heading;},
 		}
 	}
@@ -113,16 +107,16 @@ mod tests {
 
 	#[test]
 	fn test_parse_input() {
-		use Direction::{North, Right, Forward};
+		use Direction::{North, Left, Forward};
 
 		let eg_text = "F10\nN3\nF7\nR90\nF11";
 		assert_eq!(
 			parse_input(eg_text).unwrap(),
 			vec![
 				Forward(10u64),
-				North(3u64),
+				North(3i64),
 				Forward(7u64),
-				Right(90u16),
+				Left(270u16),
 				Forward(11u64),
 			],
 		)
@@ -130,13 +124,13 @@ mod tests {
 
 	#[test]
 	fn test_calc_path_offset() {
-		use Direction::{North, Right, Forward};
+		use Direction::{North, Left, Forward};
 
 		let eg_instructions = vec![
 			Forward(10u64),
-			North(3u64),
+			North(3i64),
 			Forward(7u64),
-			Right(90u16),
+			Left(270u16),
 			Forward(11u64),
 		];
 		assert_eq!(
