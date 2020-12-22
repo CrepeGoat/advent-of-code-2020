9dc9918c56704c2282414b9b69d3e83ee52fa3b9
CrepeGoat <studentoflife96@gmail.com>
2020-12-22 16:07:05 -0500

    tmp

--- AoC2020-rust/src/bin/day18part1.rs
+++ AoC2020-rust/src/bin/day18part1.rs
@@ -1,25 +1,53 @@
 use std::error::Error;
 
 
-enum MathValue<T: Copy> {
+enum MathSymbol<T: Copy> {
     Literal(T),
-    Expr(MathExpr<T>),
-}
-
-struct MathExpr<T: Copy> {
-    first: Box<MathValue<T>>,
-    ops: Vec<(Box<dyn Fn(T, T) -> T>, MathValue<T>)>,
+    Operator(Box<dyn Fn(T, T) -> T>),
+    Expr(Vec<MathSymbol<T>>),
 }
 
 
-//struct MathParseError impl Error;
+enum MathParseError {
+    InvalidChar,
+    BadParentheses,
+}
 
 
-//impl FromStr for MathExpr {
-//    type Err = MathParseError;
-//
-//    fn 
-//}
+impl<T> FromStr for MathSymbol<T> {
+    type Err = MathParseError;
+
+    #[derive(Debug)]
+    enum MathStrToken {
+        Char(char),
+        Symbol(MathSymbol),
+    }
+
+    fn parse(&self) -> Result<Self, Self::Err> {
+        let mut tokens = self.replace(" ", "").chars()
+            .map(MathStrToken::Char)
+            .map(|token| match token {
+                MathStrToken::Char('+') => MathStrToken::Symbol(MathSymbol::Operator(|x, y| x+y)),
+                MathStrToken::Char('*') => MathStrToken::Symbol(MathSymbol::Operator(|x, y| x*y)),
+                _ => _,
+            })
+            .collect();
+
+        fn parse_ungrouped(tokens: &[MathStrToken]) -> Result<Self, Self::Err> {
+
+        }
+
+        while let Some(i2) = tokens.find(MathStrToken::Char(')')) {
+            let i1 = tokens[..i2].rfind(MathStrToken::Char('('))
+                .ok_or(MathParseError::BadParentheses)?;
+            tokens[i1..=i2].replace(parse_ungrouped(tokens[i1+1..i2])?)
+        }
+        if let Some(i) = tokens.find(MathStrToken::Char('(')) {
+            return Err(MathParseError::BadParentheses));
+        }
+        parse_ungrouped(tokens)
+    }
+}
 
 impl<T: Copy> MathValue<T> {
     fn compute(&self) -> T {
