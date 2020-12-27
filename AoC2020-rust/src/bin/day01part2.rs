51920d781da265d5fcfc0a801d08f5fcec1ba642
CrepeGoat <awqatty.b@gmail.com>
2020-12-26 20:56:00 -0500

    tmp: rewriting day1p2 to first split items into bins by modulo value

--- AoC2020-rust/src/bin/day01part2.rs
+++ AoC2020-rust/src/bin/day01part2.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ord;
-use std::ops::{Add, Sub};
 use std::str::FromStr;
 use std::io::BufRead;
 
@@ -10,23 +8,58 @@ fn parsing_input<R: BufRead, T: FromStr>(reader: R) -> impl Iterator<Item=T> {
 		.filter_map(|s| s.parse::<T>().ok())
 }
 
-fn find_sum_triplet<T>(mut seq: Vec<T>, value: T) -> Option<(T, T, T)>
-	where T: Copy + Ord + Add<Output=T> + Sub<Output=T>
+fn find_sum_triplet(mut seq: Vec<u32>, value: u32) -> Option<(u32, u32, u32)>
 {
-	seq.sort_unstable();
+	let bin_count = (seq.len() as f64).sqrt() as u32;
+	let sort_key = {|a: &u32| (*a % bin_count, *a)};
 
-	(0usize..seq.len())
+	seq.sort_unstable_by_key(sort_key);
+	let mut seq_bins: Vec<&[u32]> = Vec::new();
+	seq_bins.reserve_exact(bin_count as usize);
+	for n in 0..bin_count {
+		let i = seq[..].binary_search_by_key(&(n as u32, n as u32), sort_key)
+			.map_or_else(|x| x, |x| x);
+		let j = seq[..].binary_search_by_key(&(n as u32 + 1, n as u32 + 1), sort_key)
+			.map_or_else(|x| x, |x| x);
+		seq_bins.push(&seq[i..j])
+	}
+
+	fn search_bins(value: u32, bins: (&[u32], &[u32], &[u32])) -> Option<(u32, u32, u32)>{
+		if bins.0.len() > bins.2.len() {
+			return search_bins(value, (bins.2, bins.1, bins.0));
+		}
+		if bins.1.len() > bins.2.len() {
+			return search_bins(value, (bins.2, bins.0, bins.1));
+		}
+		if bins.0.len() > bins.1.len() {
+			return search_bins(value, (bins.1, bins.0, bins.2));
+		}
+
+		(0usize..bins.0.len())
+			.flat_map(|i|
+				(i+1..bins.1.len())
+					.map(move |j| (i, j))
+					.take_while(|(i, j)| bins.0[*i] + bins.1[*j] < value)
+			)
+			.find_map(|(i, j)|
+				bins.2[j+1..]
+					.binary_search(&(value-bins.0[i]-bins.1[j]))
+					.ok()
+					.map(|k| (bins.0[i], bins.1[j], bins.2[k+j+1]))
+			)
+	}
+
+	(0usize..seq_bins.len())
 		.flat_map(|i|
-			(i+1..seq.len())
+			(i+1..seq_bins.len())
 				.map(move |j| (i, j))
 				.take_while(|(i, j)| seq[*i] + seq[*j] < value)
 		)
-		.find_map(|(i, j)|
-			seq[j+1..]
-				.binary_search(&(value-seq[i]-seq[j]))
-				.ok()
-				.map(|k| (seq[i], seq[j], seq[k+j+1]))
-		)
+		.find_map(|(i, j)| search_bins(value, (
+			seq_bins[i],
+			seq_bins[j],
+			seq_bins[(value-(i as u32)-(j as u32) % bin_count) as usize]
+		)))
 }
 
 fn main() {
